2558667145f471428948cf98e444d9b4b3fce180
Matt Bise <matthew.bise@rigup.com>
2020-10-06 12:41:14 -0500

    Add some styling

--- examples/counter/src/Counter.tsx
+++ examples/counter/src/Counter.tsx
@@ -7,10 +7,22 @@ export const Counter: React.FC = () => {
   const presenter = usePresenter(CounterPresenter, { count: 0 });
 
   return (
-    <div>
-      <span>Count: {presenter.count}</span>
-      <button onClick={() => presenter.decrement()}>Decrement</button>
-      <button onClick={() => presenter.increment()}>Increment</button>
+    <div className="counter">
+      <h2 className="counter-text">Count: {presenter.count}</h2>
+      <div className="btn-container">
+        <button
+          className="counter-btn decrement"
+          onClick={() => presenter.decrement()}
+        >
+          Decrement
+        </button>
+        <button
+          className="counter-btn increment"
+          onClick={() => presenter.increment()}
+        >
+          Increment
+        </button>
+      </div>
     </div>
   );
 };
