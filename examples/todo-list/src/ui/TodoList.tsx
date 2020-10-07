51eef3ce4f1ad44ea9b0189b347b9bc96716e63f
Matt Bise <mbise1993@gmail.com>
2020-10-06 19:38:56 -0500

    Add item on enter

--- examples/todo-list/src/ui/TodoList.tsx
+++ examples/todo-list/src/ui/TodoList.tsx
@@ -7,14 +7,22 @@ import { TodoListPresenter } from '../presentation/todoListPresenter';
 export const TodoList: React.FC = () => {
   const presenter = usePresenter(TodoListPresenter);
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      presenter.addNewItem();
+    }
+  };
+
   return (
     <div className="todo-list">
       <div className="new-item-input-container">
         <input
+          autoFocus
           className="new-item-input"
           type="text"
           value={presenter.newItemText}
           onChange={(e) => presenter.updateNewItemText(e.target.value)}
+          onKeyDown={handleKeyPress}
         />
         <button
           disabled={presenter.newItemText.length === 0}
