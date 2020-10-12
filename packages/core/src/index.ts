fe40776815474b46b88739147b78ecd7e5420d92
Matt Bise <matthew.bise@rigup.com>
2020-10-12 11:43:12 -0500

    Use emitter from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
 export { Presenter } from './presentation/presenter';
 export { View } from './presentation/view';
-export { Emitter, ReadOnlyEmitter } from './util/emitter';
+export { Emitter, ReadOnlyEmitter, emitterOf } from './util/emitter';
 export { AnyObject } from './util/types';
